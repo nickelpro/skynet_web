32cb541b5cd476c295b2350abc8eaece83952b98
Nick Gamberini <nickg@nickg.org>
2013-03-13 05:24:22 -0400

    Stupid little script

---
+++ fixskynet.py
@@ -0,0 +1,14 @@
+import psycopg2
+from dblogin import dbname, dbuser, dbpass
+
+conn = psycopg2.connect(database=dbname, user=dbuser, password=dbpass)
+cur = conn.cursor()
+
+conn.execute("""SELECT se1.player_name, se1.time, se1.online FROM skynet_events se1 INNER JOIN (
+					SELECT MAX(time) AS time, player_name FROM skynet_events GROUP BY player_name
+				) se2 ON (se1.player_name = se2.player_name AND se1.time = se2.time AND se1.online = True)"""
+)
+
+data = conn.fetchall()
+conn.commit()
+print data
